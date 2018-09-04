0bddeab0da5dcbdd59f2658c3d62be876270b4fc
SanjanaElangovan <esanvi97@gmail.com>
2018-09-04 15:35:10 +0530

    doctor register

--- src/app/doctor-login/doctor-login.component.ts
+++ src/app/doctor-login/doctor-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-doctor-login',
@@ -11,5 +13,10 @@ export class DoctorLoginComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
 }
