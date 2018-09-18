3ef6effb43332b259a06853e043ade869a75a8ab
Sivaranjani G <siva19191212@gmail.com>
2018-09-18 12:43:55 +0530

    changes made in patient and patientservice

--- src/app/patient.ts
+++ src/app/patient.ts
@@ -2,4 +2,11 @@ export class Patient {
     patientEmail:string;
     patientPhoneNumber:string;
     patientPassword:string;
+    patientFirstName:string;
+    patientLastName:string;
+    patientAddress:string;
+    patientGender:string;
+    patientDOB:Date;
+    patientBloodGroup:string;
+    patientAge:number;
 }
