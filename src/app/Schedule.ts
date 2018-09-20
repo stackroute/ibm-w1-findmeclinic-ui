dc0a0281b4eba4f7da17df7edb05fe933634f092
Tanmaya <tansri11@in.ibm.com>
2018-09-20 21:23:19 +0530

    minor update calendar

---
+++ src/app/Schedule.ts
@@ -0,0 +1,11 @@
+export class Schedule{
+    private  scheduleId: string;
+    private  description: string;
+    private  workPlace: string;
+    private  startDate: any;
+    private  endDate: any;
+    private  createdBy: string;
+    private  scheduleCreationDate: Date;
+    private  time_per_patient: any;
+    private  status: string;
+}
