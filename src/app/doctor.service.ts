2437e552ff750d6d35e3b47ff6bcb09ab21ca141
jachri11 <jachri11@in.ibm.com>
2018-09-21 09:46:24 +0530

    Seach Page Issue Solved

--- src/app/doctor.service.ts
+++ src/app/doctor.service.ts
@@ -10,12 +10,8 @@ export class DoctorService {
 
   // private addUrl = "http://172.23.239.228:8080/doctor/auth";
   private addUrl = "http://localhost:8080/doctor/auth";
-<<<<<<< HEAD
 
   private secUrl="http://localHost:8080/api/v1/search/";
-=======
-  private secUrl="http://localHost:8082/api/v2/doctor";
->>>>>>> c9bb8af461ff3e61146d72b3d5fe9594330a6887
 
 
   constructor(private http: HttpClient) { }
@@ -37,19 +33,11 @@ getByEmail(doctorEmail) :Observable<Doctor>
 {
 return this.http.get<Doctor>(this.secUrl+"/"+doctorEmail);
 }
-<<<<<<< HEAD
 getDoctorByDoctorName(doctorName:string):Observable<any>
 {
   // this.doctor = new BehaviorSubject(doctorName).asObservable();
 
   return this.http.get<any>(this.secUrl+doctorName);
-=======
-getDoctorByDoctorName(doctorName:string):Observable<Doctor[]>
-{
-//  this.doctor = new BehaviorSubject(doctorName).asObservable();
-
-  return this.http.get<Doctor[]>(this.secUrl+"/get/"+doctorName);
->>>>>>> c9bb8af461ff3e61146d72b3d5fe9594330a6887
 }
 
 addDoctorDetails(doctor:Doctor):Observable<Doctor>{
