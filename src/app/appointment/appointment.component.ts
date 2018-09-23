4ced52a54ee3ae7a95b4ad366654c72520fdc85d
Lakshmi <lakshv13>
2018-09-23 14:59:10 +0530

    Patient Profile Corrected

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -15,11 +15,6 @@ export class AppointmentComponent implements OnInit {
 
 
   displayedColumns = ['appointmentId', 'appointmentStatus'];
-<<<<<<< HEAD
-  show = false;
-=======
-
->>>>>>> 895fafc67fafd384565ebc87e804dc965d5066da
   @ViewChild(MatPaginator) paginator: MatPaginator;
   listOfAppointments: Appointment[];
   displayAppointments: Appointment[];
@@ -32,10 +27,6 @@ export class AppointmentComponent implements OnInit {
   listSize;
   diff;
   userId;
-<<<<<<< HEAD
-  patientName;
-=======
->>>>>>> 895fafc67fafd384565ebc87e804dc965d5066da
   dataSource = new MatTableDataSource<Appointment>(this.listOfAppointments);
   constructor(private router: Router, public dialog: MatDialog, private doctorId: DoctorTokenStorage, private appService: AppointmentService) {
   }
@@ -43,13 +34,10 @@ export class AppointmentComponent implements OnInit {
 
   ngOnInit() {
 
-<<<<<<< HEAD
-=======
 
 
 
 
->>>>>>> 895fafc67fafd384565ebc87e804dc965d5066da
   }
   onPage() {
     this.numOfApp = this.listSize - 1;
@@ -61,44 +49,15 @@ export class AppointmentComponent implements OnInit {
     console.log(this.displayAppointments)
 
   }
-<<<<<<< HEAD
-  open(){
-
-    this.dialog.open(PrescriptionsComponent)
-
-       }
-
-   
-  
-=======
   open() {
 
     this.dialog.open(PrescriptionsComponent)
 
   }
 
-  ngAfterViewInit() {
-    this.userId = this.doctorId.getUserId();
-
-    this.appService.getAllAppointments(this.userId).subscribe((data: Appointment[]) => {
-      this.listOfAppointments = data;
-      this.lengthlist = this.listOfAppointments.length;
-
-      this.listSize = this.lengthlist
-      this.numOfApp = this.listSize - 1;
-      this.diff = this.listSize - this.paginator.pageSize;
-      if (this.diff > 0) {
-        this.numOfApp = this.paginator.pageSize;
-      }
-      this.displayAppointments = this.listOfAppointments.slice(0, this.numOfApp);
-      console.log(this.displayAppointments)
-
-    })
-  }
+  
 
 
-
->>>>>>> 895fafc67fafd384565ebc87e804dc965d5066da
 
   ngAfterViewInit() {
     this.userId = this.doctorId.getUserId();
