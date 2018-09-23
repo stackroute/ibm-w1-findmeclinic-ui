1d4125c04510d63e1fa4d4782c9e0faaac349a09
Diwakar <diwakarabhijeet@gmail.com>
2018-09-23 21:10:10 +0530

    presList

--- src/app/prescription-list.service.ts
+++ src/app/prescription-list.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject} from 'rxjs';
+import {Prescription} from './Prescription';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrescriptionListService {
 
-  constructor() { }
+  private listUrl="http://localhost:8085/api/v1/record/patient/";
+
+  constructor(private http: HttpClient) { }
+
+  getPrescription(patientID): Observable<Prescription[]> {
+    return this.http.get<Prescription[]>(this.listUrl+patientID);
+  } 
+
 }
