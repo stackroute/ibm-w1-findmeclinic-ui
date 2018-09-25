0266bab0d5dd34cb01a56b1a9a10a3b7cb3353bd
keerthiamoli <keerthikaruna20@gmail.com>
2018-09-25 17:55:58 +0530

    changes made in prescription

--- src/app/prescriptions/prescriptions.component.ts
+++ src/app/prescriptions/prescriptions.component.ts
@@ -59,7 +59,6 @@ export class PrescriptionsComponent implements OnInit {
   prescriptions: Prescription = {
     findings: '', medicine: [''],
     patient: {
-<<<<<<< HEAD
       patientEmail:'',
       patientPhoneNumber:'',
       patientPassword:'',
@@ -69,20 +68,6 @@ export class PrescriptionsComponent implements OnInit {
       patientGender:'',
       patientBloodGroup:'',
       patientAge:''
-=======
-      patientEmail: '',
-      patientPhoneNumber: '',
-      patientPassword: '',
-      patientFirstName: '',
-      patientLastName: '',
-      patientAddress: '',
-      patientGender: '',
-      patientDobDate: '',
-      patientDobMonth: '',
-      patientDobYear: '',
-      patientBloodGroup: '',
-      patientAge: ''
->>>>>>> 8a5ff1e901b0c17461fd23b5549e88ba3f66fa76
     }
     , doctor: {
       doctorFirstName: '',
