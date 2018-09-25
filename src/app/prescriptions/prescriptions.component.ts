09170656351eecb3e7ccbfbc8bc647ba8f10e8ab
SanjanaElangovan <esanvi97@gmail.com>
2018-09-25 16:59:27 +0530

    changes in appointment

--- src/app/prescriptions/prescriptions.component.ts
+++ src/app/prescriptions/prescriptions.component.ts
@@ -37,7 +37,6 @@ export class PrescriptionsComponent implements OnInit {
     this.dialog.closeAll();
   }
   ngOnInit() {
-<<<<<<< HEAD
     this.docId = this.token.getUserId();
     this.patientId= this.data.name;
     console.log(this.data.patientName);
@@ -56,28 +55,10 @@ export class PrescriptionsComponent implements OnInit {
       this.prescriptions.patient.patientAge = this.myPatient.patientAge;
       this.prescriptions.patient.patientBloodGroup = this.myPatient.patientBloodGroup;
     })
-=======
-    this.patientNam = this.route.snapshot.params['bookingBy'];
-  this.docId = this.service.doctorId;
-   this.service.getByEmail(this.docId).subscribe(data=>{console.log(data),
-  this.myDoctor=data;
-  this.prescriptions.patient.patientEmail=this.patientNam;
-  this.prescriptions.doctor.doctorEmail=this.docId;
-  this.prescriptions.doctor.doctorName=this.myDoctor.doctorName;
-})
-this.patientService.getPatientByMail(this.patientNam).subscribe(data=>{console.log(data),
-  this.myPatient=data;
-  this.prescriptions.patient.patientFirstName=this.myPatient.patientFirstName;
-  console.log(this.myPatient.patientFirstName);
-  this.prescriptions.patient.patientAge=this.myPatient.patientAge;
-  this.prescriptions.patient.patientBloodGroup=this.myPatient.patientBloodGroup;
-})
->>>>>>> 3c4ed8da4a019804e3c17bb6f812d079ff3c4bad
   }
   prescriptions: Prescription = {
     findings: '', medicine: [''],
     patient: {
-<<<<<<< HEAD
       patientEmail:'',
       patientPhoneNumber:'',
       patientPassword:'',
@@ -87,20 +68,6 @@ this.patientService.getPatientByMail(this.patientNam).subscribe(data=>{console.l
       patientGender:'',
       patientBloodGroup:'',
       patientAge:''
-=======
-      patientEmail: '',
-      patientPhoneNumber: '',
-      patientPassword: '',
-      patientFirstName: '',
-      patientLastName: '',
-      patientAddress: '',
-      patientGender: '',
-      patientDobDate: '',
-      patientDobMonth: '',
-      patientDobYear: '',
-      patientBloodGroup: '',
-      patientAge: ''
->>>>>>> 8a5ff1e901b0c17461fd23b5549e88ba3f66fa76
     }
     , doctor: {
       doctorFirstName: '',
